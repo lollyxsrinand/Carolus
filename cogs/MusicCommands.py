4caf14da64fee16f376912b1733587f767075baa
Prince349k <71537129+Prince349k@users.noreply.github.com>
2020-11-18 22:34:06 +0530

    another

--- cogs/MusicCommands.py
+++ cogs/MusicCommands.py
@@ -452,18 +452,18 @@ class Music(commands.Cog):
         ctx.voice_state.songs.remove(index - 1)
         await ctx.message.add_reaction('✅')
 
-    @commands.command(name='loop')
-    async def _loop(self, ctx: commands.Context):
-        """Loops the currently playing song.
-        Invoke this command again to unloop the song.
-        """
+    # @commands.command(name='loop')
+    # async def _loop(self, ctx: commands.Context):
+    #     """Loops the currently playing song.
+    #     Invoke this command again to unloop the song.
+    #     """
 
-        if not ctx.voice_state.is_playing:
-            return await ctx.send('Nothing is being played at the moment.')
+    #     if not ctx.voice_state.is_playing:
+    #         return await ctx.send('Nothing is being played at the moment.')
 
-        # Inverse boolean value to loop and unloop.
-        # ctx.voice_state.loop = not ctx.voice_state.loop
-        await ctx.message.add_reaction('✅')
+    #     # Inverse boolean value to loop and unloop.
+    #     # ctx.voice_state.loop = not ctx.voice_state.loop
+    #     await ctx.message.add_reaction('✅')
 
     @commands.command(name='play',aliases=['p'])
     async def _play(self, ctx: commands.Context, *, search: str):
